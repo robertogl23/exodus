052354e568d3097f43042056cfd3da70a0815a41
robertogl23 <dev.robertgl@gmail.com>
2020-07-01 13:20:43 -0500

    url manual

--- pages/manual.js
+++ pages/manual.js
@@ -1,6 +1,21 @@
 import Head from 'next/head'
+import styled from "styled-components";
+
 import Header from '../components/Header'
-import HomeContainer from '../components/HomeContainer'
+import Link from "next/link";
+const LayoutHome = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+
+  height: 100vh !important;
+  top: 56px;
+  box-sizing: border-box;
+  @media (max-width: 967px) {
+		padding:0;
+	}
+
+`;
 export default function Manual() {
   return (
     <div className="container">
@@ -13,9 +28,13 @@ export default function Manual() {
       <Header/>
 
       <main>
-        <HomeContainer>
-            <button onClick={() => alert('Aun no esta listo :(')}>Descargar Manual</button>
-        </HomeContainer>
+        <LayoutHome>
+           <div>
+             <Link href="https://drive.google.com/file/d/1PYXFzJH9z0ao8_2y59CJZcrjLkPYy3x6/view?usp=sharing" prefetch={false}>
+             <a>Manual</a>
+             </Link>
+           </div>
+        </LayoutHome>
       </main>
 
      
